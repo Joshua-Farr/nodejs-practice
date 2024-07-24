647030385322c68ea66f0b1f70c2bdff29ccc8be
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-07-23 22:03:36 -0500

    Resolved merge conflicts

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import chalk from "chalk";
 import notes from "./notes.js";
-=======
-const getNotes = require("./notes.js");
-const validator = require("validator");
->>>>>>> f46b3d58c8344b8f4f8c3ac2e1f2593c6f4f3b7a
 
 let emailStatus = validator.isEmail("josh@josh.com");
 console.log(emailStatus);
 
 const message = getNotes();
 
-<<<<<<< HEAD
 console.log(chalk.green.inverse.bold("Success!"));
 const command = process.argv[2];
 
@@ -22,6 +16,3 @@ if (command === "add") {
 } else if (command === "remove") {
   console.log("Removing note!");
 }
-=======
-console.log(message);
->>>>>>> f46b3d58c8344b8f4f8c3ac2e1f2593c6f4f3b7a
