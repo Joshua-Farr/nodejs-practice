3239281d562fb94980055e90e4b7d5bbfe78d900
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-08-07 22:34:55 -0500

    Adding geocoding and rotating keys

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,33 @@
 const request = require("postman-request");
+require("dotenv").config();
 
-const URL = "http://api.weatherapi.com/v1/current.json?key=&q=London&aqi=no";
+const weatherKey = process.env.WEATHER_API_KEY;
+const geocodeKey = process.env.GEOCODE_API_KEY;
 
-request({ url: URL, json: true }, (error, response) => {
-  const data = response.body.current;
-  console.log(
-    `It is currently ${data.temp_f} degrees out and ${data.condition.text}!`
-  );
+const weatherURL = `http://api.weatherapi.com/v1/current.json?key=${weatherKey}&q=London&aqi=no`;
+
+request({ url: weatherURL, json: true }, (error, response) => {
+  if (error) {
+    console.log("ERROR: Unable to connect to weather service");
+  } else {
+    const data = response.body.current;
+
+    console.log(
+      `It is currently ${data.temp_f} degrees out and ${data.condition.text}!`
+    );
+  }
+});
+
+const locationURL = `https://api.mapbox.com/search/geocode/v6/forward?q=Los%20Angeles&access_token=${geocodeKey}&limit=1`;
+
+request({ url: locationURL, json: true }, (error, response) => {
+  if (error) {
+    console.log("ERROR: Unable to connect to gecoding service");
+  } else {
+    const { longitude, latitude } =
+      response.body.features[0].properties.coordinates;
+
+    console.log("Longitude: ", longitude);
+    console.log("Latitude: ", latitude);
+  }
 });
