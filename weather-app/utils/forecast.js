fa98a8b74fd3e9fa27a7a09cc3be7bba1722b5e4
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-08-10 14:39:42 -0500

    added new utils

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -6,17 +6,17 @@ const weatherKey = process.env.WEATHER_API_KEY;
 const forecast = (latitude, longitude, callback) => {
   const weatherURL = `http://api.weatherapi.com/v1/current.json?key=${weatherKey}&q=${latitude},${longitude}&aqi=no`;
 
-  request({ url: weatherURL, json: true }, (error, response) => {
+  request({ url: weatherURL, json: true }, (error, { body }) => {
     if (error) {
       callback("ERROR: Unable to connect to weather service", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("ERROR: Unable to locate, please try again", undefined);
     } else {
-      const data = response.body.current;
+      const { temp_f, condition } = body.current;
 
       callback(
         undefined,
-        `It is currently ${data.temp_f} degrees out and ${data.condition.text}!`
+        `It is currently ${temp_f} degrees out and ${condition.text}!`
       );
     }
   });
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -5,20 +5,19 @@ const geocodeKey = process.env.GEOCODE_API_KEY;
 
 const geocode = (address, callback) => {
   const locationURL = `https://api.mapbox.com/search/geocode/v6/forward?q=${address}&access_token=${geocodeKey}&limit=1`;
-  request({ url: locationURL, json: true }, (error, response) => {
+  request({ url: locationURL, json: true }, (error, { body }) => {
     if (error) {
       callback(
         "ERROR: Unable to connect to gecoding location service",
         undefined
       );
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback("Error: Unable to find locaiton, try another search", undefined);
     } else {
-      const { longitude, latitude } =
-        response.body.features[0].properties.coordinates;
+      const { longitude, latitude } = body.features[0].properties.coordinates;
 
       callback(undefined, {
-        location: response.body.features[0].properties.name,
+        location: body.features[0].properties.name,
         longitute: longitude,
         latitude: latitude,
       });
