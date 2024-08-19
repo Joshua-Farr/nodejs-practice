6fa20fe8721eec6a6b1aa7f95ebf4b180ac68b40
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-08-19 18:25:37 -0500

    Changed fetch url

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,19 +1,9 @@
 console.log("Client side JS is loaded!");
 
-// const weatherData = fetch("http://localhost:3000/weather?address=boston").then(
-//   (response) => {
-//     response.json().then((data) => {
-//       console.log(data);
-//     });
-//   }
-// );
-
 const weatherForm = document.querySelector("form");
 const searchTerm = document.querySelector("input");
 const message1 = document.querySelector("#message-1");
 const message2 = document.querySelector("#message-2");
-
-const PORT = process.env.PORT;
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -21,17 +11,15 @@ weatherForm.addEventListener("submit", (e) => {
 
   message2.textContent = "Fetching Data...";
   message1.textContent = null;
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.errorMessage) {
-          message2.textContent = null;
-          message1.textContent = data.errorMessage;
-        } else {
-          message1.textContent = data.location;
-          message2.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.errorMessage) {
+        message2.textContent = null;
+        message1.textContent = data.errorMessage;
+      } else {
+        message1.textContent = data.location;
+        message2.textContent = data.forecast;
+      }
+    });
+  });
 });
