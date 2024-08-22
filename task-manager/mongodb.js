f1301d4fcdbd85879c40108f41f07b821a271c6e
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-08-21 19:24:37 -0500

    Updating to modern async/await structure

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,12 +1,19 @@
-//CRUD create read update delete
-
-const mongodb = require("mongodb");
-
-const MongoClient = mongodb.MongoClient;
+const { MongoClient } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, () => {
-  console.log("Connected to the database!");
-});
+async function main() {
+  const client = await MongoClient.connect(connectionURL);
+  console.log("Connected successfully to server");
+
+  const db = client.db(databaseName);
+
+  db.collection("users").insertOne({
+    name: "Josh",
+    age: 31,
+  });
+}
+
+main().catch(console.error);
+//.finally(() => client.close());
